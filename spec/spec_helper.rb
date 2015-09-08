817cc9016ab1b2b3488c747dff65ae8f5e8173b2
Fabien Wernli <wernli@in2p3.fr>
2015-09-08 15:16:13 +0200

    reflect changes from 5785b6d1 in params.pp handling in fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,14 +5,30 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 }
 
 @os_facts = {
-  'RedHat'            => @common_facts.merge({
+  'RedHat6'            => @common_facts.merge({
     :operatingsystem      => 'RedHat',
+    :operatingsystemmajrelease => '6',
     :osfamily             => 'RedHat',
     :server_package_name  => 'conserver',
     :client_package_name  => 'conserver-client',
   }),
-  'Debian'            => @common_facts.merge({
+  'RedHat7'            => @common_facts.merge({
+    :operatingsystem      => 'RedHat',
+    :operatingsystemmajrelease => '7',
+    :osfamily             => 'RedHat',
+    :server_package_name  => 'conserver',
+    :client_package_name  => 'conserver-client',
+  }),
+  'Debian7'            => @common_facts.merge({
     :operatingsystem     => 'Debian',
+    :operatingsystemmajrelease => '7',
+    :osfamily            => 'Debian',
+    :server_package_name  => 'conserver-server',
+    :client_package_name  => 'conserver-client',
+  }),
+  'Debian8'            => @common_facts.merge({
+    :operatingsystem     => 'Debian',
+    :operatingsystemmajrelease => '8',
     :osfamily            => 'Debian',
     :server_package_name  => 'conserver-server',
     :client_package_name  => 'conserver-client',
