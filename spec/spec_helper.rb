d6d37b022cea502b12b6108a9375d92ff969fe53
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <wernli@in2p3.fr>
2018-04-04 15:18:58 +0200

    rubocop -a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ include RspecPuppetFacts
 
 default_facts = {
   puppetversion: Puppet.version,
-  facterversion: Facter.version,
+  facterversion: Facter.version
 }
 
 default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
@@ -30,49 +30,38 @@ RSpec.configure do |c|
 end
 
 @common_facts = {
-  :concat_basedir => '/DIR',
+  concat_basedir: '/DIR'
 }
 
 @os_facts = {
-  'RedHat6'            => @common_facts.merge({
-    :operatingsystem      => 'RedHat',
-    :operatingsystemmajrelease => '6',
-    :osfamily             => 'RedHat',
-    :server_service_name  => 'conserver',
-    :server_package_name  => 'conserver',
-    :client_package_name  => 'conserver-client',
-  }),
-  'RedHat7'            => @common_facts.merge({
-    :operatingsystem      => 'RedHat',
-    :operatingsystemmajrelease => '7',
-    :osfamily             => 'RedHat',
-    :server_service_name  => 'conserver',
-    :server_package_name  => 'conserver',
-    :client_package_name  => 'conserver-client',
-  }),
-  'Debian7'            => @common_facts.merge({
-    :operatingsystem     => 'Debian',
-    :operatingsystemmajrelease => '7',
-    :osfamily            => 'Debian',
-    :server_service_name => 'conserver-server',
-    :server_package_name  => 'conserver-server',
-    :client_package_name  => 'conserver-client',
-  }),
-  'Debian8'            => @common_facts.merge({
-    :operatingsystem     => 'Debian',
-    :operatingsystemmajrelease => '8',
-    :osfamily            => 'Debian',
-    :server_service_name => 'conserver-server',
-    :server_package_name  => 'conserver-server',
-    :client_package_name  => 'conserver-client',
-  }),
-  'Ubuntu12.04'        => @common_facts.merge({
-    :operatingsystem     => 'Ubuntu',
-    :operatingsystemmajrelease => '12.04',
-    :osfamily            => 'Debian',
-    :server_service_name => 'conserver-server',
-    :server_package_name  => 'conserver-server',
-    :client_package_name  => 'conserver-client',
-  }),
+  'RedHat6' => @common_facts.merge(operatingsystem: 'RedHat',
+                                   operatingsystemmajrelease: '6',
+                                   osfamily: 'RedHat',
+                                   server_service_name: 'conserver',
+                                   server_package_name: 'conserver',
+                                   client_package_name: 'conserver-client'),
+  'RedHat7' => @common_facts.merge(operatingsystem: 'RedHat',
+                                   operatingsystemmajrelease: '7',
+                                   osfamily: 'RedHat',
+                                   server_service_name: 'conserver',
+                                   server_package_name: 'conserver',
+                                   client_package_name: 'conserver-client'),
+  'Debian7' => @common_facts.merge(operatingsystem: 'Debian',
+                                   operatingsystemmajrelease: '7',
+                                   osfamily: 'Debian',
+                                   server_service_name: 'conserver-server',
+                                   server_package_name: 'conserver-server',
+                                   client_package_name: 'conserver-client'),
+  'Debian8' => @common_facts.merge(operatingsystem: 'Debian',
+                                   operatingsystemmajrelease: '8',
+                                   osfamily: 'Debian',
+                                   server_service_name: 'conserver-server',
+                                   server_package_name: 'conserver-server',
+                                   client_package_name: 'conserver-client'),
+  'Ubuntu12.04' => @common_facts.merge(operatingsystem: 'Ubuntu',
+                                       operatingsystemmajrelease: '12.04',
+                                       osfamily: 'Debian',
+                                       server_service_name: 'conserver-server',
+                                       server_package_name: 'conserver-server',
+                                       client_package_name: 'conserver-client')
 }
-
