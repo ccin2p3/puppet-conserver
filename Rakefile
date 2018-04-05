364253975c63d94f6a6532fc8dcd2447c2591c33
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <wernli@in2p3.fr>
2018-04-05 14:58:58 +0200

    rubocop

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/rake_tasks'
-require 'puppet-lint/tasks/puppet-lint'   
+require 'puppet-lint/tasks/puppet-lint'
 
 PuppetLint.configuration.fail_on_warnings
 PuppetLint.configuration.send('disable_80chars')
@@ -11,9 +11,9 @@ ignore_paths = ['vendor/**/*', 'spec/**/*', 'smoke/**/*', 'pkg/**/*']
 
 PuppetSyntax.exclude_paths = ignore_paths
 
-#PuppetLint.configuration.ignore_paths = ignore_paths
+# PuppetLint.configuration.ignore_paths = ignore_paths
 PuppetLint::RakeTask.new :lint do |config|
   config.ignore_paths = ignore_paths
 end
 
-task :default => [:spec, :lint]
+task default: %i[spec lint]
--- spec/classes/systemd_spec.rb
+++ spec/classes/systemd_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 os_fixtures = @os_fixtures
 
 describe 'conserver', type: 'class' do
-  ['RedHat7','Debian8','Debian9'].each do |osname|
+  %w[RedHat7 Debian8 Debian9].each do |osname|
     osfixtures = os_fixtures[osname]
-    
+
     context "On os `#{osname}`" do
       let :facts do
         osfixtures[:facts]
@@ -14,9 +14,9 @@ describe 'conserver', type: 'class' do
       it { should contain_systemd__unit_file("#{osfixtures[:params][:server_service_name]}.service") }
     end
   end
-  ['RedHat6','Debian7','Ubuntu12.04'].each do |osname|
+  ['RedHat6', 'Debian7', 'Ubuntu12.04'].each do |osname|
     osfixtures = os_fixtures[osname]
-    
+
     context "On os `#{osname}`" do
       let :facts do
         osfixtures[:facts]
